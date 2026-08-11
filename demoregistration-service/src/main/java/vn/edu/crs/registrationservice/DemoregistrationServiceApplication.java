a271f839eaa1a1f25592d90beb912d45e08e49e8
xinanhdung333 <2311062573@hunre.edu.vn>
2026-08-11 10:44:44 +0700

    ok

---
+++ demoregistration-service/src/main/java/vn/edu/crs/registrationservice/DemoregistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.crs.registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoregistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoregistrationServiceApplication.class, args);
+    }
+
+}
